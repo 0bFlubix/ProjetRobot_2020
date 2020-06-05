a68ac0c1a1189e221a813a48066fb55dc88acee7
Florent <florent.barthelemy@protonmail.com>
2020-06-05 20:25:14 +0200

    restruct [STABLE_VERSION]
    features:
    RX, CB_RX
    TX, CB_RX
    reception state machine on both pc and µc side
    implemented higher level functions to send speed/led commands via uart
    +Added Blocks file

---
+++ Interface[UI_ONLY]/PortSupervisor/SerialPortSupervisor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.IO.Ports;
+
+/// <summary>
+/// not implemented yet, this class will be used for port supervision, handling port disconnected events, new port available events
+/// and changing ports without error...
+/// </summary>
+
+namespace PortSupervisor
+{
+
+    public class SerialPortSupervisor
+    {
+       
+    }
+}
